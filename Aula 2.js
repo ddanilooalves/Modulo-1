1ef5daba8301d91a65725504fbedf23fd54e6114
ddanilooalves <97872437+ddanilooalves@users.noreply.github.com>
2022-01-20 22:54:19 -0300

    Projeto 1

--- Aula 2.js
+++ Aula 2.js
@@ -2,78 +2,77 @@ console.clear()
 var prompt = require(`prompt-sync`)();
 
 console.log()
-
-console.log(`Escolha seu cavaleiro ou cavaleira...!`);
-
-console.log()
-console.log(`1 - Yennifer`);
-console.log(`2 - Hanna`);
-console.log(`3 - Athreus`);
-console.log(`4 - Lufyn`);
-console.log()
-
-let res = +prompt(`Cavaleiro(a): `);
-console.log()
-
-if (res === 1)
-{
-    console.log(`Em um reino bem distante, chamado Horrara, uma cavaleira chamada Yennifer, destemida e muito forte, lutava para se tornar a próxima rainha!
-Mas para que ela se tornasse uma rainha, ela deveria provar que é merecedora, concluindo a missão que lhe foi imposta!
-Yennifer então saiu na sua jornada para completar sua missão...`)
-console.log()
-console.log(`Você passará pelo monte dos gigantes para tentar recuperar a espada dos Deuses. Será que você obterá sucesso nesse desafio? 
-Logo depois você enfrentará a cobra gigante de Mumbanda, para conseguir a Pedra da Vida!
-O próximo desáfio é nos confins do deserto de Aramboca, onde você terá que pegar a Capa de Invisibilidade!
-O seu próximo desáfio é pelas campinas de Dorck, a procura do Cavalo Branco de Thor!
-E por último e não menos importante, você terá que passar por Valarrary, para recuperar A Poção de Cura!
-Agora responda cavaleira Yennifer com Sim ou Não... `)
-console.log()
-}
-
-if(res == 2)
-{
-    console.log(`Em um reino bem distante, chamado Horrara, uma cavaleira chamada Hanna, destemida e muito forte, lutava para se tornar a próxima rainha!
+    
+    console.log(`Escolha seu cavaleiro ou cavaleira...!`);
+    
+    console.log()
+    console.log(`1 - Yennifer`);
+    console.log(`2 - Hanna`);
+    console.log(`3 - Athreus`);
+    console.log(`4 - Lufyn`);
+    console.log()
+    
+    let res = +prompt(`Cavaleiro(a): `);
+    console.log()
+    
+    if (res === 1)
+    {
+        console.log(`Em um reino bem distante, chamado Horrara, uma cavaleira chamada Yennifer, destemida e muito forte, lutava para se tornar a próxima rainha!
     Mas para que ela se tornasse uma rainha, ela deveria provar que é merecedora, concluindo a missão que lhe foi imposta!
-    Hannad então saiu na sua jornada para completar sua missão...`)
+    Yennifer então saiu na sua jornada para completar sua missão...`)
     console.log()
     console.log(`Você passará pelo monte dos gigantes para tentar recuperar a espada dos Deuses. Será que você obterá sucesso nesse desafio? 
     Logo depois você enfrentará a cobra gigante de Mumbanda, para conseguir a Pedra da Vida!
     O próximo desáfio é nos confins do deserto de Aramboca, onde você terá que pegar a Capa de Invisibilidade!
     O seu próximo desáfio é pelas campinas de Dorck, a procura do Cavalo Branco de Thor!
     E por último e não menos importante, você terá que passar por Valarrary, para recuperar A Poção de Cura!
-    Agora responda cavaleira Hanna com Sim ou Não... `)
+    Agora responda cavaleira Yennifer com Sim ou Não... `)
     console.log()
-} 
-
-if (res == 3)
-{
-    console.log(`Em um reino bem distante, chamado Horrara, um cavaleiro cahamado Athreus, destemido e muito forte, lutava para se tornar o próximo rei!
-Mas para que ele se tornasse o rei, ele deveria provar que era merecedor, concluindo a missão que lhe foi imposta!
-O cavaleiro então saiu na sua jornada para completar sua missão...`)
-console.log()
-console.log(`Você passará pelo monte dos gigantes para tentar recuperar a espada dos Deuses. Será que você obterá sucesso nesse desafio? 
-Logo depois você enfrentará a cobra gigante de Mumbanda, para conseguir a Pedra da Vida!
-O próximo desáfio é nos confins do deserto de Aramboca, onde você terá que pegar a Capa de Invisibilidade!
-O seu próximo desáfio é pelas campinas de Dorck, a procura do Cavalo Branco de Thor!
-E por último e não menos importante, você terá que passar por Valarrary, para recuperar A Poção de Cura!
-Agora responda cavaleiro Athreus com Sim ou Não... `)
-console.log()
-}
-
-if (res == 4);
-{
-    console.log(`Em um reino bem distante, chamado Horrara, um cavaleiro cahamado Lufyn, destemido e muito forte, lutava para se tornar o próximo rei!
-Mas para que ele se tornasse o rei, ele deveria provar que era merecedor, concluindo a missão que lhe foi imposta!
-O cavaleiro então saiu na sua jornada para completar sua missão...`)
-console.log()
-console.log(`Você passará pelo monte dos gigantes para tentar recuperar a espada dos Deuses. Será que você obterá sucesso nesse desafio? 
-Logo depois você enfrentará a cobra gigante de Mumbanda, para conseguir a Pedra da Vida!
-O próximo desáfio é nos confins do deserto de Aramboca, onde você terá que pegar a Capa de Invisibilidade!
-O seu próximo desáfio é pelas campinas de Dorck, a procura do Cavalo Branco de Thor!
-E por último e não menos importante, você terá que passar por Valarrary, para recuperar A Poção de Cura!
-Agora responda cavaleiro Lufyn com Sim ou Não... `)
-console.log()
-}
+    }
+    
+    if(res == 2)
+    {
+        console.log(`Em um reino bem distante, chamado Horrara, uma cavaleira chamada Hanna, destemida e muito forte, lutava para se tornar a próxima rainha!
+        Mas para que ela se tornasse uma rainha, ela deveria provar que é merecedora, concluindo a missão que lhe foi imposta!
+        Hannad então saiu na sua jornada para completar sua missão...`)
+        console.log()
+        console.log(`Você passará pelo monte dos gigantes para tentar recuperar a espada dos Deuses. Será que você obterá sucesso nesse desafio? 
+        Logo depois você enfrentará a cobra gigante de Mumbanda, para conseguir a Pedra da Vida!
+        O próximo desáfio é nos confins do deserto de Aramboca, onde você terá que pegar a Capa de Invisibilidade!
+        O seu próximo desáfio é pelas campinas de Dorck, a procura do Cavalo Branco de Thor!
+        E por último e não menos importante, você terá que passar por Valarrary, para recuperar A Poção de Cura!
+        Agora responda cavaleira Hanna com Sim ou Não... `)
+        console.log()
+    } 
+    
+    if (res == 3)
+    {
+        console.log(`Em um reino bem distante, chamado Horrara, um cavaleiro cahamado Athreus, destemido e muito forte, lutava para se tornar o próximo rei!
+    Mas para que ele se tornasse o rei, ele deveria provar que era merecedor, concluindo a missão que lhe foi imposta!
+    O cavaleiro então saiu na sua jornada para completar sua missão...`)
+    console.log()
+    console.log(`Você passará pelo monte dos gigantes para tentar recuperar a espada dos Deuses. Será que você obterá sucesso nesse desafio? 
+    Logo depois você enfrentará a cobra gigante de Mumbanda, para conseguir a Pedra da Vida!
+    O próximo desáfio é nos confins do deserto de Aramboca, onde você terá que pegar a Capa de Invisibilidade!
+    O seu próximo desáfio é pelas campinas de Dorck, a procura do Cavalo Branco de Thor!
+    E por último e não menos importante, você terá que passar por Valarrary, para recuperar A Poção de Cura!
+    Agora responda cavaleiro Athreus com Sim ou Não... `)
+    console.log()
+    }
+    
+    if (res == 4)
+    {
+        console.log(`Em um reino bem distante, chamado Horrara, um cavaleiro cahamado Lufyn, destemido e muito forte, lutava para se tornar o próximo rei!
+    Mas para que ele se tornasse o rei, ele deveria provar que era merecedor, concluindo a missão que lhe foi imposta!
+    O cavaleiro então saiu na sua jornada para completar sua missão...`)
+        console.log()
+        console.log(`Você passará pelo monte dos gigantes para tentar recuperar a espada dos Deuses. Será que você obterá sucesso nesse desafio? 
+    Logo depois você enfrentará a cobra gigante de Mumbanda, para conseguir a Pedra da Vida!
+    O próximo desáfio é nos confins do deserto de Aramboca, onde você terá que pegar a Capa de Invisibilidade!
+    O seu próximo desáfio é pelas campinas de Dorck, a procura do Cavalo Branco de Thor!
+    E por último e não menos importante, você terá que passar por Valarrary, para recuperar A Poção de Cura!
+    Agora responda cavaleiro Athreus com Sim ou Não... `)
+    }
 
 var cont = 0
 
