69b21ce6f4f0335cb7303bb52262065f7fe4b35a
ddanilooalves <97872437+ddanilooalves@users.noreply.github.com>
2022-01-21 19:36:43 -0300

    Aula 1

---
+++ Aula 1.js
@@ -0,0 +1,75 @@
+
+var prompt = require(`prompt-sync`)();
+
+
+// let idade = 19;
+// const cidade = `Goiânia`;
+// let nome = "Danilo";
+
+// console.log("Tenho", idade, "anos e moro em", cidade);
+
+// nome = `Danilo`;
+// idade = 19;
+
+// console.log(nome);
+// console.log(idade);
+
+// idade = idade + 10
+// console.log(cidade + idade);
+
+// console.log(`Olá ${nome}, tudo bem? Você tem ${idade} anos!`);
+
+// let string = `Blue EdTech`;
+// let numberInt = 10;
+// let numberFloat = 10.5;
+// let boolean = true;
+
+// console.log(string);
+// console.log(numberInt);
+// console.log(numberFloat);
+// console.log(boolean);
+// console.log(typeof(string));
+
+// const cidade = prompt("Qual cidade você mora? ");
+// console.log(`Que legal, você mora em ${cidade}`);
+// let numero = prompt(`Quantos km somado com dez? `);
+// console.log(numero + 10);
+// let numero = +prompt(`Quantos km somado com dez? `);
+// console.log(numero + 10);
+// console.log(Number(numero) + 10);
+
+// const num1 = +prompt('Digite um número: ');
+// const num2 = +prompt('Digite outro número: ');
+
+// console.log(num1 + num2);
+
+// const nome = prompt('Digite seu nome: ');
+// const sobrenome = prompt('Digite seu sobrenome: ')
+
+// console.log(nome + sobrenome);
+
+
+// const num1 = +prompt('Digite um número: ');
+// const num2 = +prompt('Digite outro número: ');
+
+// console.log(`Subtração: ${num1} - ${num2} = ${num1 - num2}`);
+// console.log(`Multiplicação: ${num1} * ${num2} = ${num1 * num2}`); // Em JavaScript usamos um * para multiplicação
+// console.log(`Divisão: ${num1} / ${num2} = ${num1 / num2}`);
+// console.log(`Resto da divisão de ${num1}/${num2}: ${num1 % num2}`);
+// console.log(`Resultado de ${num1} elevado a ${num2}: ${num1 ** num2}`);
+
+/*Exercícios 1
+
+// let numA = +prompt("Digita sua nota 1: ");
+// let numB = +prompt("Digite sua nota 2: ")
+
+// console.log("Sua nota final é", (numA + numB) /2);,
+*/
+
+
+
+let peca1 = prompt(`Quantas peças de camiseta você deseja? `)
+
+if (peca1 <= 1000); {
+    
+}
